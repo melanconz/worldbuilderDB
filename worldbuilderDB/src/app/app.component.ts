fd9550efcd6c6d2b36f4a5be4d419a9cd7797994
Zachary Melancon <zmelan01@gmail.com>
2016-09-20 11:12:45 -0500

    angular 2 tutorial stuff

--- worldbuilderDB/src/app/app.component.ts
+++ worldbuilderDB/src/app/app.component.ts
@@ -1,10 +1,34 @@
+const CREATIONS: Creation[] = [
+  { id: 11, name: 'Dante' },
+  { id: 12, name: 'Blake' },
+  { id: 13, name: 'Wordsworth' },
+  { id: 14, name: 'Shelley' },
+  { id: 15, name: 'Byron' },
+  { id: 16, name: 'Chaucer' },
+  { id: 17, name: 'Kant' },
+  { id: 18, name: 'Goethe' },
+  { id: 19, name: 'Proust' },
+  { id: 20, name: 'Lawrence' }
+];
+
 import { Component } from '@angular/core';
+
+export class Creation{
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'app works!';
+  creations = CREATIONS;
+  title = 'WorldBuilderDB';
+  selectedCreation: Creation;
+  onSelect(creation: Creation): void {
+      this.selectedCreation = creation;
+  }
 }
